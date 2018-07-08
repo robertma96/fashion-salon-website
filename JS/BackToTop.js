d48b482daca7cc8a543c4bd55092a9cf9f3ce75f
Nicolae Robert Marasescu <robert.marasescu@yahoo.com>
2018-07-08 17:20:05 +0300

    ADD solar, manichiura, cosmetica
    First final version

---
+++ JS/BackToTop.js
@@ -0,0 +1,28 @@
+// BY KAREN GRIGORYAN
+
+$(document).ready(function () {
+    // declare variable
+    var scrollTop = $(".scrollTop");
+
+    $(window).scroll(function () {
+        // declare variable
+        var topPos = $(this).scrollTop();
+
+        // if user scrolls down - show scroll to top button
+        if (topPos > 600) {
+            $(scrollTop).css("opacity", "1");
+
+        } else {
+            $(scrollTop).css("opacity", "0");
+        }
+
+    }); // scroll END
+
+    //Click event to scroll to top
+    $(scrollTop).click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    }); // click() scroll top EMD
+}); // ready() END
